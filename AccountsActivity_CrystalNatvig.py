98b73515be7f918e246852685fff531165699e13
natvigc <54685344+natvigc@users.noreply.github.com>
2019-09-12 12:24:15 -0600

    Create AccountsActivity_CrystalNatvig.py

---
+++ AccountsActivity_CrystalNatvig.py
@@ -0,0 +1,11 @@
+Learn more or give us feedback
+
+
+def test_activity_zero():
+    moviequote = '... that some birds aren't meant to be caged.'
+    print(moviequote)
+    assert False
+
+    
+def test_pull_request_add():
+    assert True
